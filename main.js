2c8363dc3296d3e65d4f77ca6af6932ae95ad058
Nestor Rios Garcia <neriga10@hotmail.com>
2024-02-25 16:07:04 -0600

    Clase 5: Shallow copy en JavaScript

--- main.js
+++ main.js
@@ -1,48 +1,18 @@
-console.log("Hello World");
-
-const juan = {
-  name: "Juan",
-  age: 25,
-  approvedCourses: ["Curso 1", "Curso 2"],
-  addCourse(newCourse) {
-    console.log("this", this);
-    console.log("this.approvedCourses", this.approvedCourses);
-    this.approvedCourses.push(newCourse);
+const obj1 = {
+  a: "a",
+  b: "b",
+  c: {
+    d: "d",
+    e: "e",
   },
 };
 
-// juan.addCourse("Curso 3");
+const obj2 = {};
 
-// console.log(Object.keys(juan));
-// console.log(Object.getOwnPropertyNames(juan));
-// console.log(Object.entries(juan));
+for (prop in obj1) {
+  obj2[prop] = obj1[prop];
+}
 
-// Object.defineProperty(juan, "Navigator", {
-//   value: "Chrome",
-//   enumerable: false,
-//   writable: true,
-//   configurable: true,
-// });
-// Object.defineProperty(juan, "editor", {
-//   value: "VSCode",
-//   enumerable: true,
-//   writable: false,
-//   configurable: true,
-// });
-// Object.defineProperty(juan, "terminal", {
-//   value: "WSL",
-//   enumerable: true,
-//   writable: true,
-//   configurable: false,
-// });
-// Object.defineProperty(juan, "pruebaNasa", {
-//   value: "Extraterrestres",
-//   enumerable: false,
-//   writable: false,
-//   configurable: false,
-// });
-
-// Object.seal(juan);
-Object.freeze(juan);
-
-console.log(Object.getOwnPropertyDescriptors(juan));
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
+// const obj4 = {...obj1}
