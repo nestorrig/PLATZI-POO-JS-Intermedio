09653c46bbe17d0cc9e46ab07d47a7d2f1fa1358
Nestor Rios Garcia <neriga10@hotmail.com>
2024-03-03 01:14:32 -0600

    Clase 7: Qué es JSON.parse y JSON.stringify

--- main.js
+++ main.js
@@ -7,12 +7,5 @@ const obj1 = {
   },
 };
 
-const obj2 = {};
-
-for (prop in obj1) {
-  obj2[prop] = obj1[prop];
-}
-
-const obj3 = Object.assign({}, obj1);
-const obj4 = Object.create(obj1);
-// const obj4 = {...obj1}
+const stringifiedComplexObject = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObject);
